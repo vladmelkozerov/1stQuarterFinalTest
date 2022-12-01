0b635171ff6348c5d59a4b387a01e27fe0c18b8d
Влад <seeker13@mail.ru>
2022-12-01 22:59:55 +0500

    1st commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// //Итоговая проверочная работа за первую четверть
+string [] array1=new string[5];
+string [] array2=new string[5];
+int j=0;
+for (int i=0;i<array1.Length;i++)
+    {   
+        Console.WriteLine($"Введите {i+1}-й элемент массива строк");
+        array1[i]=Console.ReadLine()!;
+        if (array1[i].Length<=3)      
+            {
+                array2[j]=array1[i];
+                j++;
+            }
+    }
